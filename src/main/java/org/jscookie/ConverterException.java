6e715c9b4bed672ef9009e9102dd171aa2347ae0
Fagner Brack <github@fagnermartins.com>
2015-04-29 20:42:58 -0300

    This exception should only be used inside a converter when there's error

--- src/main/java/org/jscookie/ConverterException.java
+++ src/main/java/org/jscookie/ConverterException.java
@@ -1,6 +1,6 @@
 package org.jscookie;
 
-public class ConverterException extends Exception {
+public final class ConverterException extends Exception {
 	private static final long serialVersionUID = 1;
 	public ConverterException( Throwable cause ) {
 		super( cause );
