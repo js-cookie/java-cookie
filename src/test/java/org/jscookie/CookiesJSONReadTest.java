30e52016ab10c0dddf2bef0f301f6dc74553743d
Fagner Brack <github@fagnermartins.com>
2015-04-29 22:15:45 -0300

    Create several tests for properties in custom types

--- src/test/java/org/jscookie/CookiesJSONReadTest.java
+++ src/test/java/org/jscookie/CookiesJSONReadTest.java
@@ -67,7 +67,7 @@ public class CookiesJSONReadTest extends BaseTest {
 //	}
 //
 	@Test
-	public void read_custom_type() throws ParseException {
+	public void read_custom_type_with_string_prop() throws ParseException {
 		Mockito.when( request.getCookies() ).thenReturn(new Cookie[] {
 			new Cookie( "c", "{\"property\":\"v\"}" )
 		});
@@ -76,8 +76,38 @@ public class CookiesJSONReadTest extends BaseTest {
 		String expected = "{\"property\":\"v\"}";
 		Assert.assertEquals( expected, actual );
 
-		String actual2 = cookies.get( "c", CustomType.class ).getProperty();
+		String actual2 = cookies.get( "c", CustomTypeString.class ).getProperty();
 		String expected2 = "v";
+		Assert.assertEquals( expected2, actual2 );
+	}
+
+	@Test
+	public void read_custom_type_with_boolean_prop() throws ParseException {
+		Mockito.when( request.getCookies() ).thenReturn(new Cookie[] {
+			new Cookie( "c", "{\"property\":true}" )
+		});
+
+		String actual = cookies.get( "c" );
+		String expected = "{\"property\":true}";
+		Assert.assertEquals( expected, actual );
+
+		Boolean actual2 = cookies.get( "c", CustomTypeBoolean.class ).getProperty();
+		Boolean expected2 = true;
+		Assert.assertEquals( expected2, actual2 );
+	}
+
+	@Test
+	public void read_custom_type_with_number_prop() throws ParseException {
+		Mockito.when( request.getCookies() ).thenReturn(new Cookie[] {
+			new Cookie( "c", "{\"property\":1}" )
+		});
+
+		String actual = cookies.get( "c" );
+		String expected = "{\"property\":1}";
+		Assert.assertEquals( expected, actual );
+
+		Integer actual2 = cookies.get( "c", CustomTypeInteger.class ).getProperty();
+		Integer expected2 = 1;
 		Assert.assertEquals( expected2, actual2 );
 	}
 
@@ -96,10 +126,26 @@ public class CookiesJSONReadTest extends BaseTest {
 //		Assert.assertEquals( "v", actual.getString( "v" ) );
 //	}
 
-	private static class CustomType {
+	private static class CustomTypeString {
 		private String property;
 		@JsonProperty( "property" )
 		private String getProperty() {
+			return property;
+		}
+	}
+
+	private static class CustomTypeBoolean {
+		private Boolean property;
+		@JsonProperty( "property" )
+		private Boolean getProperty() {
+			return property;
+		}
+	}
+
+	private static class CustomTypeInteger {
+		private Integer property;
+		@JsonProperty( "property" )
+		private Integer getProperty() {
 			return property;
 		}
 	}
