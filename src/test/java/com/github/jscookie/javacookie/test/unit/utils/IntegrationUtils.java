6c8e0721229ac9865412e5c21e3342e440ac3248
Thomas Lutz <lutz@symptoma.com>
2019-02-25 20:21:30 +0100

    Update Maven/NPM depdendencies, remove Grunt (not necessary)

--- src/test/java/com/github/jscookie/javacookie/test/unit/utils/IntegrationUtils.java
+++ src/test/java/com/github/jscookie/javacookie/test/unit/utils/IntegrationUtils.java
@@ -1,26 +1,26 @@
 package com.github.jscookie.javacookie.test.unit.utils;
 
-import java.io.File;
-
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 
+import java.io.File;
+
 public class IntegrationUtils {
-	public static WebArchive createCommonDeployment() {
-		boolean RECURSIVE_TRUE = true;
-		return ShrinkWrap.create( WebArchive.class )
-			.addPackage( "com.github.jscookie.javacookie" )
-			.addPackages( RECURSIVE_TRUE, "com.github.jscookie.javacookie.test.integration" )
-			.addAsLibraries(
-				Maven.resolver()
-					.loadPomFromFile( "pom.xml" )
-					.resolve(
-						"joda-time:joda-time",
-						"com.fasterxml.jackson.core:jackson-databind"
-					)
-					.withTransitivity()
-					.as( File.class )
-			);
-	}
+  public static WebArchive createCommonDeployment() {
+    boolean RECURSIVE_TRUE = true;
+    return ShrinkWrap.create(WebArchive.class)
+        .addPackage("com.github.jscookie.javacookie")
+        .addPackages(RECURSIVE_TRUE, "com.github.jscookie.javacookie.test.integration")
+        .addAsLibraries(
+            Maven.resolver()
+                .loadPomFromFile("pom.xml")
+                .resolve(
+                    "joda-time:joda-time",
+                    "com.fasterxml.jackson.core:jackson-databind"
+                )
+                .withTransitivity()
+                .as(File.class)
+        );
+  }
 }
