91a7265b377a6c1a6ae08684994dc58771d29e7b
Thomas Lutz <lutz@symptoma.com>
2019-02-25 20:25:52 +0100

    Add failing test

--- src/test/java/com/github/jscookie/javacookie/test/unit/CookiesDecodingTest.java
+++ src/test/java/com/github/jscookie/javacookie/test/unit/CookiesDecodingTest.java
@@ -1,5 +1,7 @@
 package com.github.jscookie.javacookie.test.unit;
 
+import com.github.jscookie.javacookie.Cookies;
+import com.github.jscookie.javacookie.test.unit.utils.BaseTest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,31 +9,36 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import com.github.jscookie.javacookie.Cookies;
-import com.github.jscookie.javacookie.test.unit.utils.BaseTest;
-
-@RunWith( MockitoJUnitRunner.class )
+@RunWith(MockitoJUnitRunner.class)
 public class CookiesDecodingTest extends BaseTest {
-	private Cookies cookies;
+  private Cookies cookies;
 
-	@Before
-	public void before() {
-		cookies = Cookies.initFromServlet( request, response );
-	}
+  @Before
+  public void before() {
+    cookies = Cookies.initFromServlet(request, response);
+  }
 
-	@Test
-	public void character_not_allowed_in_name_and_value() {
-		Mockito.when( request.getHeader( "cookie" ) ).thenReturn( "%3B=%3B" );
-		String actual = cookies.get( ";" );
-		String expected = ";";
-		Assert.assertEquals( expected, actual );
-	}
+  @Test
+  public void character_not_allowed_in_name_and_value() {
+    Mockito.when(request.getHeader("cookie")).thenReturn("%3B=%3B");
+    String actual = cookies.get(";");
+    String expected = ";";
+    Assert.assertEquals(expected, actual);
+  }
 
-	@Test
-	public void character_with_3_bytes() {
-		Mockito.when( request.getHeader( "cookie" ) ).thenReturn( "c=%E4%BA%AC" );
-		String actual = cookies.get( "c" );
-		String expected = "京";
-		Assert.assertEquals( expected, actual );
-	}
+  @Test
+  public void character_with_3_bytes() {
+    Mockito.when(request.getHeader("cookie")).thenReturn("c=%E4%BA%AC");
+    String actual = cookies.get("c");
+    String expected = "京";
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void two_encoded_characters() {
+    Mockito.when(request.getHeader("cookie")).thenReturn("c=New%20York%2C%20NY");
+    String actual = cookies.get("c");
+    String expected = "New York, NY";
+    Assert.assertEquals(expected, actual);
+  }
 }
